7632c904d7e10777ac0d085460bd9a43a3cc39dc
Taybh <tayebehb92@gmail.com>
2026-05-11 18:45:03 +0200

    add the txt policy data

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,7 @@
 def read_and_split_text(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         text = file.read()
-    paragraphs = text.split('\n')
+    paragraphs = text.split("\n")
     # Filter out any empty paragraphs or undesired entries
-    paragraphs = [para.strip() for para in paragraphs if len(para.strip()) > 0]
+    paragraphs = [p.strip() for p in paragraphs if len(p.strip()) > 0]
     return paragraphs
